45856af5ff1dac694922b0b5254edba2f4a735c3
ingemar.finn <ingemar.finn@king.com>
2023-09-22 11:20:04 +0200

    Add views

---
+++ blipp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlippConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blipp'
--- blipp/urls.py
+++ blipp/urls.py
@@ -19,7 +19,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("blipp/", include("blipp.urls")),
     path("", views.index, name="index"),
-    path('admin/', admin.site.urls),
 ]
